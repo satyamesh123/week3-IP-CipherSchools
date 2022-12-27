866a6929dc0adb0ae0bdb08189d609b4d939faa3
Satyam Maheshwari <118844893+satyamesh123@users.noreply.github.com>
2022-12-28 00:13:53 +0530

    Add files via upload

---
+++ class7.js
@@ -0,0 +1,16 @@
+// Modifying the elements & attrubutes in JS
+let attribute =document.querySelector("img").attributes;
+console.log(attribute);
+
+let hasattribute = document.querySelector("img").hasAttribute("border");
+console.log(hasattribute);
+
+let getattribute= document.querySelector("img").getAttribute("src");
+console.log("getattribute");
+
+let setattribute =document.querySelector("img").setAttribute("src" , "image_river.jpeg");
+// console.log(setattribute);
+
+let removeattribute =document.querySelector("img").removeAttribute("src" , "image_river.jpeg");
+// console.log(removeattribute);
+
